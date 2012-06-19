a7b7fa40706b91239630e9b33e0bc15120c965da
michelc <michelc@gmail.com>
2012-06-19 21:12:19 +0200

    Mise en évidence des zones de saisie obligatoire via un helper
    (en remplacement du code jQuery)

---
+++ Repertoir/Helpers/HtmlHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace Repertoir.Helpers
+{
+    public static class HtmlHelperExtension
+    {
+        // http://blog.orangelightning.co.uk/?p=20
+        // http://weblogs.asp.net/gunnarpeipman/archive/2012/06/17/asp-net-mvc-how-to-show-asterisk-after-required-field-label.aspx
+        public static MvcHtmlString CaptionFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression)
+        {
+            var metadata = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
+            string htmlFieldName = ExpressionHelper.GetExpressionText(expression);
+            string labelText = metadata.DisplayName ?? metadata.PropertyName ?? htmlFieldName.Split('.').Last();
+            if (String.IsNullOrEmpty(labelText))
+            {
+                return MvcHtmlString.Empty;
+            }
+
+            var tag = new TagBuilder("label");
+            tag.Attributes.Add("for", html.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldId(htmlFieldName));
+
+            bool isRequired = false;
+            if (metadata.ContainerType != null)
+            {
+                isRequired = metadata.ContainerType.GetProperty(metadata.PropertyName)
+                                     .GetCustomAttributes(typeof(RequiredAttribute), false)
+                                     .Length == 1;
+            }
+
+            if (isRequired)
+            {
+                tag.AddCssClass("is_required");
+                tag.InnerHtml = String.Format("{0}<span>*</span>", labelText);
+            }
+            else
+            {
+                tag.SetInnerText(labelText);
+            }
+
+            return MvcHtmlString.Create(tag.ToString(TagRenderMode.Normal));
+        }
+
+    }
+}
