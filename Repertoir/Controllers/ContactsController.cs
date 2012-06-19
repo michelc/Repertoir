2754bf0d8e2078e0f65fbee6a148dee88b05a0f0
michelc <michelc@gmail.com>
2012-06-19 22:39:08 +0200

    Utilisation d'un contrôleur eople pour gérer les personnes
    (en prévision d'un autre contrôleur Companies pour gérer les sociétés)

--- Repertoir/Controllers/ContactsController.cs
+++ Repertoir/Controllers/ContactsController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 using Repertoir.Models;
 
@@ -30,84 +25,6 @@ namespace Repertoir.Controllers
                             }).ToList();
 
             return View(contacts);
-        }
-
-        //
-        // GET: /Contacts/Details/5
-
-        public ViewResult Details(int id)
-        {
-            Contact contact = db.Contacts.Find(id);
-            return View(contact);
-        }
-
-        //
-        // GET: /Contacts/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        } 
-
-        //
-        // POST: /Contacts/Create
-
-        [HttpPost, ValidateAntiForgeryToken]
-        public ActionResult Create(Contact contact)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Contacts.Add(contact);
-                db.SaveChanges();
-                return RedirectToAction("Index");  
-            }
-
-            return View(contact);
-        }
-        
-        //
-        // GET: /Contacts/Edit/5
- 
-        public ActionResult Edit(int id)
-        {
-            Contact contact = db.Contacts.Find(id);
-            return View(contact);
-        }
-
-        //
-        // POST: /Contacts/Edit/5
-
-        [HttpPost, ValidateAntiForgeryToken]
-        public ActionResult Edit(Contact contact)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(contact).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(contact);
-        }
-
-        //
-        // GET: /Contacts/Delete/5
- 
-        public ActionResult Delete(int id)
-        {
-            Contact contact = db.Contacts.Find(id);
-            return View(contact);
-        }
-
-        //
-        // POST: /Contacts/Delete/5
-
-        [HttpPost, ValidateAntiForgeryToken, ActionName("Delete")]
-        public ActionResult DeleteConfirmed(int id)
-        {            
-            Contact contact = db.Contacts.Find(id);
-            db.Contacts.Remove(contact);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
---
+++ Repertoir/Controllers/PeopleController.cs
@@ -0,0 +1,101 @@
+﻿using System.Data;
+using System.Web.Mvc;
+using Repertoir.Models; 
+
+namespace Repertoir.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private RepertoirContext db = new RepertoirContext();
+
+        //
+        // GET: /People/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Contact person = db.Contacts.Find(id);
+
+            return View(person);
+        }
+
+        //
+        // GET: /People/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /People/Create
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Create(Contact person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(person);
+                db.SaveChanges();
+
+                return RedirectToAction("Details", new { Id = person.ID });
+            }
+
+            return View(person);
+        }
+
+        //
+        // GET: /People/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            Contact person = db.Contacts.Find(id);
+
+            return View(person);
+        }
+
+        //
+        // POST: /People/Edit/5
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Edit(Contact person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Details", new { Id = person.ID });
+            }
+            return View(person);
+        }
+
+        //
+        // GET: /People/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            Contact person = db.Contacts.Find(id);
+
+            return View(person);
+        }
+
+        //
+        // POST: /People/Delete/5
+
+        [HttpPost, ValidateAntiForgeryToken, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact person = db.Contacts.Find(id);
+            db.Contacts.Remove(person);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Contacts");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
