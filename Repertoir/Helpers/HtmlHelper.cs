fc450dca82951610a2d0d3774daa2bea75b32b14
michelc <michelc@gmail.com>
2012-06-23 09:47:20 +0200

    Ajout d'un helper pour les liens CRUD

--- Repertoir/Helpers/HtmlHelper.cs
+++ Repertoir/Helpers/HtmlHelper.cs
@@ -61,5 +61,96 @@ namespace Repertoir.Helpers
 
             return html;
         }
+
+        public static HtmlString ActionCrud(this HtmlHelper helper, int id = 0)
+        {
+            var current_action = helper.ViewContext.RouteData.Values["action"].ToString().ToLower();
+
+            var html = "";
+
+            // Si on n'est pas sur la page Index
+            if (current_action != "index")
+            {
+                // Alors, il faut un lien vers la page Index
+                html += helper.ActionLink("Contacts", "Index", "Contacts").ToString();
+            }
+            else
+            {
+                // Sinon, il n'y a pas besoin d'un lien vers la page Index
+                html += "Contacts";
+            }
+
+            // Si on n'est pas sur la page Create
+            html += " / ";
+            if (current_action != "create")
+            {
+                // Alors, il faut un lien vers la page Create
+                var current_controller = helper.ViewContext.RouteData.Values["controller"].ToString().ToLower();
+                // Si on est sur l'index des contacts
+                if (current_controller == "contacts")
+                {
+                    // Alors, le lien Create doit se faire sur les personnes
+                    html += helper.ActionLink("Créer", "Create", "People").ToString();
+                }
+                else
+                {
+                    // Sinon, le lien Create doit se faire sur le controlleur en cours
+                    html += helper.ActionLink("Créer", "Create").ToString();
+                }
+            }
+            else
+            {
+                // Sinon, il n'y a pas besoin d'un lien vers la page Create
+                html += "Créer";
+            }
+
+            // Si on a un identifiant de fiche
+            if (id != 0)
+            {
+                // Alors, il faut générer les autres liens CRUD
+
+                // Si on n'est pas sur la page Details
+                html += " / ";
+                if (current_action != "details")
+                {
+                    // Alors, il faut un lien vers la page Details
+                    html += helper.ActionLink("Afficher", "Details", new { id = id }).ToString();
+                }
+                else
+                {
+                    // Sinon, il n'y a pas besoin d'un lien vers la page Details
+                    html += "Afficher";
+                }
+
+                // Si on n'est pas sur la page Edit
+                html += " / ";
+                if (current_action != "edit")
+                {
+                    // Alors, il faut un lien vers la page Edit
+                    html += helper.ActionLink("Modifier", "Edit", new { id = id }).ToString();
+                }
+                else
+                {
+                    // Sinon, il n'y a pas besoin d'un lien vers la page Edit
+                    html += "Modifier";
+                }
+
+                // Si on n'est pas sur la page Delete
+                html += " / ";
+                if (current_action != "delete")
+                {
+                    // Alors, il faut un lien vers la page Delete
+                    html += helper.ActionLink("Supprimer", "Delete", new { id = id }).ToString();
+                }
+                else
+                {
+                    // Sinon, il n'y a pas besoin d'un lien vers la page Delete
+                    html += "Supprimer";
+                }
+
+            }
+
+            return new HtmlString(html);
+        }
     }
 }
