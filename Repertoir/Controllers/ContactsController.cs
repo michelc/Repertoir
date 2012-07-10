60595fc838819d5dd5ceccf762634f1505661b8d
michelc <michelc@gmail.com>
2012-07-10 08:07:46 +0200

    Ajout d'un export basique au format JSON

--- Repertoir/Controllers/ContactsController.cs
+++ Repertoir/Controllers/ContactsController.cs
@@ -30,6 +30,39 @@ namespace Repertoir.Controllers
             return View(contacts);
         }
 
+        //
+        // GET: /Contacts/Export
+
+        public JsonResult Export()
+        {
+            var contacts = (from c in db.Contacts
+                            orderby c.IsCompany descending, c.DisplayName ascending
+                            select new
+                            {
+                                Slug = c.Slug,
+                                DisplayName = c.DisplayName,
+                                IsCompany = c.IsCompany,
+                                CompanyName = c.Company_ID.HasValue ? c.Company.CompanyName : "",
+                                Civility = c.Civility,
+                                LastName = c.LastName,
+                                FirstName = c.FirstName,
+                                Title = c.Title,
+                                Phone1 = c.Phone1,
+                                Phone2 = c.Phone2,
+                                Fax = c.Fax,
+                                Email = c.Email,
+                                Url = c.Url,
+                                AddressLine1 = c.AddressLine1,
+                                AddressLine2 = c.AddressLine2,
+                                PostalCode = c.PostalCode,
+                                Municipality = c.Municipality,
+                                Country = c.Country,
+                                Notes = c.Notes
+                            }).ToList();
+
+            return Json(contacts, JsonRequestBehavior.AllowGet);
+        }
+
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
