387871c92ca1b31366f81d2b521aef607d034e0a
michelc <michelc@gmail.com>
2012-07-14 14:13:12 +0200

    Ajout de tests unitaires pour le helper Flash()

---
+++ Repertoir.Tests/Helpers/FlashHelperTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repertoir.Controllers;
+using Repertoir.Helpers;
+
+namespace Repertoir.Tests.Helpers
+{
+    [TestClass]
+    public class FlashHelperTest
+    {
+        [TestMethod]
+        public void Flash_enregistre_le_texte_dans_TempData()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            controller.Flash("Raton-laveur");
+
+            // Assert
+            Assert.IsTrue(controller.TempData.ContainsValue("Raton-laveur"));
+        }
+
+        [TestMethod]
+        public void Flash_renvoie_null_quand_pas_de_message()
+        {
+            // Arrange            
+            var controller = new HomeController();
+            var context = new ViewContext
+            {
+                TempData = controller.TempData
+            };
+            HtmlHelper helper = new HtmlHelper(context, new VdcImplementation());
+
+            // Act
+            var flash = helper.Flash();
+
+            // Assert
+            Assert.AreEqual(null, flash);
+        }
+
+        [TestMethod]
+        public void Flash_renvoie_le_message_quand_il_existe()
+        {
+            // Arrange            
+            var controller = new HomeController();
+            controller.Flash("Raton-laveur");
+            var context = new ViewContext
+            {
+                TempData = controller.TempData
+            };
+            HtmlHelper helper = new HtmlHelper(context, new VdcImplementation());
+
+            // Act
+            var flash = helper.Flash();
+
+            // Assert
+            Assert.IsTrue(flash.ToString().Contains("Raton-laveur"));
+        }
+
+        private class VdcImplementation : IViewDataContainer
+        {
+            public ViewDataDictionary ViewData { get; set; }
+        }
+    }
+}
