604f18f90d37e2a3fa975b86c6f1811b7fb2a554
michelc <michelc@gmail.com>
2012-08-22 20:26:17 +0200

    Mise à jour de EntityFramework 4.3.1 à 5.0

--- Repertoir/Models/RepertoirModels.cs
+++ Repertoir/Models/RepertoirModels.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
 
