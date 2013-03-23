5524e3a0ac9c0e93a0eb6c83c8afa2d3b8596085
michelc <michelc@gmail.com>
2013-03-23 09:41:00 +0100

    Gestion des tags à l'import Json

--- Repertoir/Controllers/ContactsController.cs
+++ Repertoir/Controllers/ContactsController.cs
@@ -113,19 +113,40 @@ namespace Repertoir.Controllers
             // Vide la table des contacts actuels
             db.Database.ExecuteSqlCommand("DELETE FROM Contacts WHERE Company_ID IS NOT NULL");
             db.Database.ExecuteSqlCommand("DELETE FROM Contacts WHERE Company_ID IS NULL");
+            db.Database.ExecuteSqlCommand("DELETE FROM Contacts_Tags");
+            db.Database.ExecuteSqlCommand("DELETE FROM Tags");
             // Réinitialise la numérotation automatique
             try
             {
                 // SQL Server CE
                 db.Database.ExecuteSqlCommand("ALTER TABLE Contacts ALTER COLUMN Contact_ID IDENTITY (1,1)");
+                db.Database.ExecuteSqlCommand("ALTER TABLE Tags ALTER COLUMN Tag_ID IDENTITY (1,1)");
             }
             catch { }
             try
             {
                 // SQL Server pas CE
                 db.Database.ExecuteSqlCommand("TRUNCATE TABLE Contacts");
+                db.Database.ExecuteSqlCommand("TRUNCATE TABLE Tags");
             }
             catch { }
+
+            // Enregistre les tags
+            var tags = new string[0];
+            foreach (var c in contacts)
+            {
+                if (!string.IsNullOrEmpty(c.Tags))
+                {
+                    tags = tags.Concat(c.Tags.Split(',')).Distinct().ToArray();
+                }
+            }
+            foreach (var t in tags.OrderBy(t => t))
+            {
+                var tag = new Tag();
+                tag.Caption = t;
+                db.Tags.Add(tag);
+            }
+            db.SaveChanges();
 
             // Enregistre les sociétés
             foreach (var c in contacts)
@@ -134,6 +155,14 @@ namespace Repertoir.Controllers
                 {
                     var contact = new Contact();
                     contact.Update_With_FlatContact(c);
+
+                    if (!string.IsNullOrEmpty(c.Tags))
+                    {
+                        tags = c.Tags.Split(',');
+                        contact.Tags = (from t in db.Tags
+                                        where tags.Contains(t.Caption)
+                                        select t).ToList();
+                    }
 
                     db.Contacts.Add(contact);
                 }
@@ -156,6 +185,14 @@ namespace Repertoir.Controllers
                         {
                             contact.Company_ID = company.Contact_ID;
                         }
+                    }
+
+                    if (!string.IsNullOrEmpty(c.Tags))
+                    {
+                        tags = c.Tags.Split(',');
+                        contact.Tags = (from t in db.Tags
+                                        where tags.Contains(t.Caption)
+                                        select t).ToList();
                     }
 
                     db.Contacts.Add(contact);
