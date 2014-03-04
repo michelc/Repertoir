09cb73e125e26e15bbfaeefd78850367facc64a1
michelc <michelc@gmail.com>
2014-03-04 20:54:06 +0100

    Remplacement de MvcRouteUnitTester par MvcRouteTester.

--- Repertoir.Tests/RoutesTest.cs
+++ Repertoir.Tests/RoutesTest.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MvcRouteUnitTester;
-using System.Web.Routing;
+﻿using System.Web.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcRouteTester;
 
 namespace Repertoir.Tests
 {
@@ -13,18 +13,17 @@ namespace Repertoir.Tests
             // Arrange
             var routes = new RouteCollection();
             RouteConfig.RegisterRoutes(routes);
-            var tester = new RouteTester(routes);
-            // var tester = new RouteTester<MvcApplication>();
 
             // Assert
-            tester.WithIncomingRequest("/").ShouldMatchRoute("Contacts", "Index");
-            tester.WithIncomingRequest("/Foo").ShouldMatchRoute("Foo", "Index");
-            tester.WithIncomingRequest("/Foo/Index").ShouldMatchRoute("Foo", "Index");
-            tester.WithIncomingRequest("/Foo/Bar").ShouldMatchRoute("Foo", "Bar");
-            tester.WithIncomingRequest("/Foo/Bar/5").ShouldMatchRoute("Foo", "Bar", new { id = 5 });
-            tester.WithIncomingRequest("/Foo/Bar/5/Baz").ShouldMatchRoute("Foo", "Bar", new { id = 5, slug = "Baz" });
-            tester.WithIncomingRequest("/Foo/Bar/5/Baz/5").ShouldMatchNoRoute();
-            tester.WithIncomingRequest("/handler.axd/pathInfo").ShouldBeIgnored();
+            RouteAssert.HasRoute(routes, "/", new { controller = "Contacts", action = "Index" });
+            RouteAssert.HasRoute(routes, "/Contacts", new { controller = "Contacts", action = "Index" });
+            RouteAssert.HasRoute(routes, "/Foo", new { controller = "Foo", action = "Index" });
+            RouteAssert.HasRoute(routes, "/Foo/Index", new { controller = "Foo", action = "Index" });
+            RouteAssert.HasRoute(routes, "/Foo/Bar", new { controller = "Foo", action = "Bar" });
+            RouteAssert.HasRoute(routes, "/Foo/Bar/5", new { controller = "Foo", action = "Bar", id = 5 });
+            RouteAssert.HasRoute(routes, "/Foo/Bar/5/Baz", new { controller = "Foo", action = "Bar", id = 5, slug = "Baz" });
+            RouteAssert.NoRoute(routes, "/Foo/Bar/5/Baz/5");
+            RouteAssert.IsIgnoredRoute(routes, "/handler.axd/pathInfo");
         }
 
         [TestMethod]
@@ -33,15 +32,14 @@ namespace Repertoir.Tests
             // Arrange
             var routes = new RouteCollection();
             RouteConfig.RegisterRoutes(routes);
-            var tester = new RouteTester(routes);
-            // var tester = new RouteTester<MvcApplication>();
 
             // Assert
-            tester.WithRouteInfo("Contacts", "Index").ShouldGenerateUrl("/");
-            tester.WithRouteInfo("Foo", "Bar").ShouldGenerateUrl("/foo/bar");
-            tester.WithRouteInfo("Foo", "Bar", new { id = 5 }).ShouldGenerateUrl("/foo/bar/5");
-            tester.WithRouteInfo("Foo", "Bar", new { id = 5, slug = "Baz" }).ShouldGenerateUrl("/foo/bar/5/baz");
-            tester.WithRouteInfo("Foo", "Bar", new { id = 5, slug = "Baz", someKey = "someValue" }).ShouldGenerateUrl("/foo/bar/5/baz?someKey=someValue");
+            RouteAssert.GeneratesActionUrl(routes, "/", new { controller = "Contacts", action = "Index" });
+            RouteAssert.GeneratesActionUrl(routes, "/Foo", new { controller = "Foo", action = "Index" });
+            RouteAssert.GeneratesActionUrl(routes, "/Foo/Bar", new { controller = "Foo", action = "Bar" });
+            RouteAssert.GeneratesActionUrl(routes, "/Foo/Bar/5", new { controller = "Foo", action = "Bar", id = 5 });
+            RouteAssert.GeneratesActionUrl(routes, "/Foo/Bar/5/Baz", new { controller = "Foo", action = "Bar", id = 5, slug = "Baz" });
+            RouteAssert.GeneratesActionUrl(routes, "/Foo/Bar/5/Baz?someKey=someValue", new { controller = "Foo", action = "Bar", id = 5, slug = "Baz", someKey = "someValue" });
         }
     }
 }
