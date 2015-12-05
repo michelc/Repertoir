f2f9918fb8622e919c3116e27d7721a483aec58a
michelc <michelc@gmail.com>
2015-12-05 09:58:33 +0100

    Correction faute d'orthographe (controller prend 2 l).

--- Repertoir/Helpers/FlashHelper.cs
+++ Repertoir/Helpers/FlashHelper.cs
@@ -4,9 +4,9 @@ namespace Repertoir.Helpers
 {
     public static class FlashHelper
     {
-        public static void Flash(this Controller controler, object text, params object[] args)
+        public static void Flash(this Controller controller, object text, params object[] args)
         {
-            controler.TempData["FlashKey"] = string.Format(text.ToString(), args);
+            controller.TempData["FlashKey"] = string.Format(text.ToString(), args);
         }
 
         public static MvcHtmlString Flash(this HtmlHelper helper)
